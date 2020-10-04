ec92fa0af9ae769c2f50b9410df98a601d7379ad
FilipeIceland <60696444+FilipeIceland@users.noreply.github.com>
2020-10-04 15:43:10 +0200

    R app data exploration

---
+++ sds_challenges_october.R
@@ -0,0 +1,144 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(ggcorrplot)
+library(corrplot)
+df <- read.csv("data/public_cars.csv")
+df_pred <- read.csv("data/pred_cars.csv")
+all_names <- names(df)
+target_name <- 'price_usd'
+var_names <- all_names[all_names != target_name]
+num_names <- var_names[unlist(lapply(df[,var_names], is.numeric))]
+n_num <- length(num_names)
+n_num2 <- n_num +1
+cat_names <- var_names[!(var_names %in% num_names)]
+n_cat <- length(cat_names)
+all_num_names <- c(num_names,target_name)
+df_all_num <- df[,all_num_names]
+corr_num <- cor(df_all_num)
+
+
+  
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Car challenge"),
+    
+    navbarPage("My Application",
+        tabPanel("Categorical variables",
+            sidebarLayout(
+                sidebarPanel(                        
+                    selectInput(
+                            inputId = "selectCatID",
+                            label = "Select categorical variable",
+                            choices = cat_names
+                            ),
+                    tableOutput(outputId = 'missingCatID')
+                    ),
+                mainPanel(
+                    plotOutput(outputId = "catbarPlot"),
+                    plotOutput(outputId = "catboxPlot")
+                    )
+                )
+            ),  
+        tabPanel("Numerical variables",
+                 sidebarLayout(
+                     sidebarPanel(
+                         selectInput(
+                             inputId = "selectNumID",
+                             label = "Select categorical variable",
+                             choices = num_names
+                         ),
+                         tableOutput(outputId = 'missingNumID'),
+                         plotOutput(outputId = "corrPlot")
+                         
+                     ),
+                     mainPanel(
+                         plotOutput(outputId = "numlinePlot"),
+                         plotOutput(outputId = "numPlot")
+                     )
+                )        
+        ),
+        tabPanel("F-Statistic",
+                 actionButton(inputId = "buttonID",
+                              label = "Show F-Statistic"),
+                 plotOutput(outputId = "fPlot", height = 700)
+        ),
+        tabPanel("Pivot tables",
+                 sidebarPanel(
+                   radioButtons(
+                      inputId = "catradiobox1ID",
+                      label = "First category",
+                      choices = cat_names,
+                      selected =  cat_names[5]),
+                   width = 2),
+                   sidebarPanel(
+                     radioButtons(
+                     inputId = "catradiobox2ID",
+                     label = "Second category",
+                     choices = cat_names,
+                     selected = cat_names[7]),
+                     width = 2),
+                 mainPanel(
+                   tableOutput(outputId = "pivottableID")
+                 )
+        )
+    )
+)
+
+
+server <- function(input, output) {
+    
+    output$missingCatID <- renderTable({
+        missing_cat <- cbind(column = cat_names, missing = sapply(df[,cat_names], function(x) sum(is.na(x))))
+        missing_cat
+    })
+    
+    output$missingNumID <- renderTable({
+        missing_num <- cbind(column = c(num_names,target_name), missing = sapply(df_all_num, function(x) sum(is.na(x))))
+        missing_num
+    })
+    
+    output$catbarPlot <- renderPlot({ ggplot(data = df, aes_(as.name(input$selectCatID), fill = as.name(input$selectCatID))) + geom_bar()})
+    
+    output$catboxPlot <- renderPlot({ ggplot(df, aes_(as.name(input$selectCatID), as.name(target_name), fill = as.name(input$selectCatID))) + geom_boxplot()})
+    
+    output$numlinePlot <- renderPlot({ggplot(data = df, aes_(as.name(input$selectNumID))) +  geom_density(colour = "red")})
+    
+    output$numPlot <- renderPlot({ggplot(data = df, aes_(as.name(input$selectNumID), as.name(target_name))) + geom_point(colour = "red")})
+    
+    output$corrPlot <- renderPlot({ ggcorrplot(corr_num, type = "upper",lab = TRUE, digits = 2)})
+    
+    
+    
+    paov <- eventReactive(input$buttonID, {
+      n_comb = n_cat*n_num2
+      p_aov<-data.frame(matrix(rep(0, n_comb), ncol = n_num2))
+      rownames(p_aov) <- cat_names
+      colnames(p_aov) <- all_num_names
+      c <- 1
+      for (i in 1:n_cat) {
+        cat.name <- cat_names[i]
+        for (j in 1:n_num2) {
+          num.name <- all_num_names[j]
+          cc <- showNotification(paste0(cat.name, '&', num.name, " ", toString(c), "/", toString(n_comb)), duration = 25)
+          myformula <- formula(paste(num.name, "~", cat.name, sep=""))
+          my.aov <- summary(aov(myformula, df, na.action=na.omit))
+          p_aov[i,j] <- round(my.aov[[1]][[5]][1], 4)
+          c <- c + 1
+        }
+        
+      }
+      ggcorrplot(t(p_aov),lab = TRUE, digits = 3)
+    })
+    
+    output$fPlot <- renderPlot({paov() })
+    
+    output$pivottableID <- renderTable({
+      pivot_table <- df %>% group_by_(input$catradiobox1ID, input$catradiobox2ID) %>% summarize(count =  n())
+      pivot_table
+    })
+}
+
+
+shinyApp(ui = ui, server = server)
